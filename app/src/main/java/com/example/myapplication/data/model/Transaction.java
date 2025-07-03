d5dc7890e86c342cfc5fbe3c857bf51834e1ac17
Võ Văn Quang <quangvo5803@gmail.com>
2025-07-03 15:00:43 +0700

    [QuangVV - 2025-07-03] Add Icon And Database

---
+++ app/src/main/java/com/example/myapplication/data/model/Transaction.java
@@ -0,0 +1,33 @@
+package com.example.myapplication.data.model;
+
+public class Transaction {
+    private int id;
+    private String type; // "income" hoặc "expense"
+    private String category;
+    private double amount;
+    private String date;
+    private String note;
+
+    public Transaction(int id, String type, String category, double amount, String date, String note) {
+        this.id = id;
+        this.type = type;
+        this.category = category;
+        this.amount = amount;
+        this.date = date;
+        this.note = note;
+    }
+
+    public int getId() { return id; }
+    public String getType() { return type; }
+    public String getCategory() { return category; }
+    public double getAmount() { return amount; }
+    public String getDate() { return date; }
+    public String getNote() { return note; }
+
+    public void setId(int id) { this.id = id; }
+    public void setType(String type) { this.type = type; }
+    public void setCategory(String category) { this.category = category; }
+    public void setAmount(double amount) { this.amount = amount; }
+    public void setDate(String date) { this.date = date; }
+    public void setNote(String note) { this.note = note; }
+}
